dd082169cb6fa9fb5e7f38ee73adc9e264c00b36
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2019-11-30 20:24:55 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ fi
   
   #wget https://github.com/wagerr/Wagerr-Blockchain-Snapshots/releases/download/Block-826819/826819.zip -O bootstrap.zip
   export fileid=1VqdvSvolhpwOoYgaoSHkZkmRla2kl27R
-  export filename=wagerr-3.1.0-x86_64-linux-gnu.tar.gz
+  export filename=monkey-2.3.1-x86_64-linux-gnu.tar.gz
   wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
      | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
 
@@ -59,12 +59,12 @@ fi
 
   wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-  tar xvzf wagerr-3.1.0-x86_64-linux-gnu.tar.gz
+  tar xvzf monkey-2.3.1-x86_64-linux-gnu.tar.gz
   
   
-  chmod +x wagerr-3.1.0/bin/*
-  sudo mv  wagerr-3.1.0/bin/* /usr/local/bin
-  rm -rf wagerr-3.1.0-x86_64-linux-gnu.tar.gz
+  chmod +x monkey-2.3.1/bin/*
+  sudo mv  monkey-2.3.1/bin/* /usr/local/bin
+  rm -rf monkey-2.3.1-x86_64-linux-gnu.tar.gz
 
   sudo apt install -y ufw
   sudo ufw allow ssh/tcp
@@ -107,7 +107,7 @@ for i in `seq 1 1 $MNCOUNT`; do
   echo "The RPC port is $RPCPORT"
 
   ALIAS=${ALIAS}
-  CONF_DIR=~/.wagerr_$ALIAS
+  CONF_DIR=~/.monkey_$ALIAS
   
   #fallocate -l 1.5G /swapfile$ALIAS
   #chmod 600 /swapfile$ALIAS
@@ -117,120 +117,49 @@ for i in `seq 1 1 $MNCOUNT`; do
   #echo "/swapfile$ALIAS none swap sw 0 0" >> /etc/fstab
 
   # Create scripts
-  echo '#!/bin/bash' > ~/bin/wagerrd_$ALIAS.sh
-  echo "wagerrd -daemon -conf=$CONF_DIR/wagerr.conf -datadir=$CONF_DIR "'$*' >> ~/bin/wagerrd_$ALIAS.sh
-  echo '#!/bin/bash' > ~/bin/wagerr-cli_$ALIAS.sh
-  echo "wagerr-cli -conf=$CONF_DIR/wagerr.conf -datadir=$CONF_DIR "'$*' >> ~/bin/wagerr-cli_$ALIAS.sh
-  echo '#!/bin/bash' > ~/bin/wagerr-tx_$ALIAS.sh
-  echo "wagerr-tx -conf=$CONF_DIR/wagerr.conf -datadir=$CONF_DIR "'$*' >> ~/bin/wagerr-tx_$ALIAS.sh 
-  chmod 755 ~/bin/wagerr*.sh
+  echo '#!/bin/bash' > ~/bin/monkeyd_$ALIAS.sh
+  echo "monkeyd -daemon -conf=$CONF_DIR/monkey.conf -datadir=$CONF_DIR "'$*' >> ~/bin/monkeyd_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/monkey-cli_$ALIAS.sh
+  echo "monkey-cli -conf=$CONF_DIR/monkey.conf -datadir=$CONF_DIR "'$*' >> ~/bin/monkey-cli_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/monkey-tx_$ALIAS.sh
+  echo "monkey-tx -conf=$CONF_DIR/monkey.conf -datadir=$CONF_DIR "'$*' >> ~/bin/monkey-tx_$ALIAS.sh 
+  chmod 755 ~/bin/monkey*.sh
 
   mkdir -p $CONF_DIR
   unzip  bootstrap.zip -d $CONF_DIR
-  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> wagerr.conf_TEMP
-  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> wagerr.conf_TEMP
-  echo "rpcallowip=127.0.0.1" >> wagerr.conf_TEMP
-  echo "rpcport=$RPCPORT" >> wagerr.conf_TEMP
-  echo "listen=1" >> wagerr.conf_TEMP
-  echo "server=1" >> wagerr.conf_TEMP
-  echo "daemon=1" >> wagerr.conf_TEMP
-  echo "logtimestamps=1" >> wagerr.conf_TEMP
-  echo "maxconnections=256" >> wagerr.conf_TEMP
-  echo "masternode=1" >> wagerr.conf_TEMP
-  echo "" >> wagerr.conf_TEMP
-  echo "addnode=101.160.14.230" >> wagerr.conf_TEMP
-  echo "addnode=104.248.38.16" >> wagerr.conf_TEMP
-  echo "addnode=108.61.223.36" >> wagerr.conf_TEMP
-  echo "addnode=110.47.182.205" >> wagerr.conf_TEMP
-  echo "addnode=115.188.31.96" >> wagerr.conf_TEMP
-  echo "addnode=128.199.184.113" >> wagerr.conf_TEMP
-  echo "addnode=134.209.148.133" >> wagerr.conf_TEMP
-  echo "addnode=134.209.155.64" >> wagerr.conf_TEMP
-  echo "addnode=134.209.28.174" >> wagerr.conf_TEMP
-  echo "addnode=139.99.159.178" >> wagerr.conf_TEMP
-  echo "addnode=142.93.132.177" >> wagerr.conf_TEMP
-  echo "addnode=142.93.148.215" >> wagerr.conf_TEMP
-  echo "addnode=142.93.156.97" >> wagerr.conf_TEMP
-  echo "addnode=142.93.230.225" >> wagerr.conf_TEMP
-  echo "addnode=149.90.37.197" >> wagerr.conf_TEMP
-  echo "addnode=157.230.245.81" >> wagerr.conf_TEMP
-  echo "addnode=159.65.53.49" >> wagerr.conf_TEMP
-  echo "addnode=159.69.195.149" >> wagerr.conf_TEMP
-  echo "addnode=159.89.169.209" >> wagerr.conf_TEMP
-  echo "addnode=163.172.51.207" >> wagerr.conf_TEMP
-  echo "addnode=165.22.17.223" >> wagerr.conf_TEMP
-  echo "addnode=165.22.195.243" >> wagerr.conf_TEMP
-  echo "addnode=165.22.217.176" >> wagerr.conf_TEMP
-  echo "addnode=165.22.232.137" >> wagerr.conf_TEMP
-  echo "addnode=165.22.234.242" >> wagerr.conf_TEMP
-  echo "addnode=165.22.58.21" >> wagerr.conf_TEMP
-  echo "addnode=167.71.208.148" >> wagerr.conf_TEMP
-  echo "addnode=167.71.222.120" >> wagerr.conf_TEMP
-  echo "addnode=167.71.62.63" >> wagerr.conf_TEMP
-  echo "addnode=167.71.72.217" >> wagerr.conf_TEMP
-  echo "addnode=167.71.72.30" >> wagerr.conf_TEMP
-  echo "addnode=167.99.138.63" >> wagerr.conf_TEMP
-  echo "addnode=167.99.185.183" >> wagerr.conf_TEMP
-  echo "addnode=170.250.57.160" >> wagerr.conf_TEMP
-  echo "addnode=172.83.8.194" >> wagerr.conf_TEMP
-  echo "addnode=173.239.232.165" >> wagerr.conf_TEMP
-  echo "addnode=178.128.117.6" >> wagerr.conf_TEMP
-  echo "addnode=178.128.174.170" >> wagerr.conf_TEMP
-  echo "addnode=178.128.29.104" >> wagerr.conf_TEMP
-  echo "addnode=178.62.111.122" >> wagerr.conf_TEMP
-  echo "addnode=178.62.30.244" >> wagerr.conf_TEMP
-  echo "addnode=178.76.223.124" >> wagerr.conf_TEMP
-  echo "addnode=185.80.130.54" >> wagerr.conf_TEMP
-  echo "addnode=188.166.22.50" >> wagerr.conf_TEMP
-  echo "addnode=188.42.252.253" >> wagerr.conf_TEMP
-  echo "addnode=193.28.233.91" >> wagerr.conf_TEMP
-  echo "addnode=198.199.75.101" >> wagerr.conf_TEMP
-  echo "addnode=206.189.125.215" >> wagerr.conf_TEMP
-  echo "addnode=206.189.53.236" >> wagerr.conf_TEMP
-  echo "addnode=209.250.248.176" >> wagerr.conf_TEMP
-  echo "addnode=213.142.96.220" >> wagerr.conf_TEMP
-  echo "addnode=213.194.177.65" >> wagerr.conf_TEMP
-  echo "addnode=24.27.40.198" >> wagerr.conf_TEMP
-  echo "addnode=45.76.221.227" >> wagerr.conf_TEMP
-  echo "addnode=46.101.183.58" >> wagerr.conf_TEMP
-  echo "addnode=47.185.158.205" >> wagerr.conf_TEMP
-  echo "addnode=68.183.192.142" >> wagerr.conf_TEMP
-  echo "addnode=68.183.193.228" >> wagerr.conf_TEMP
-  echo "addnode=68.183.198.193" >> wagerr.conf_TEMP
-  echo "addnode=68.183.2.165" >> wagerr.conf_TEMP
-  echo "addnode=68.183.219.48" >> wagerr.conf_TEMP
-  echo "addnode=71.202.226.56" >> wagerr.conf_TEMP
-  echo "addnode=75.83.187.235" >> wagerr.conf_TEMP
-  echo "addnode=85.165.245.64" >> wagerr.conf_TEMP
-  echo "addnode=90.185.118.67" >> wagerr.conf_TEMP
-  echo "addnode=91.204.138.4" >> wagerr.conf_TEMP
-  echo "addnode=91.204.190.190" >> wagerr.conf_TEMP
-  echo "addnode=95.183.51.133" >> wagerr.conf_TEMP
-  echo "addnode=95.216.26.28" >> wagerr.conf_TEMP
-  echo "addnode=95.216.46.167" >> wagerr.conf_TEMP
-  echo "addnode=95.216.74.6" >> wagerr.conf_TEMP
-  echo "addnode=95.217.62.35" >> wagerr.conf_TEMP
-  echo "" >> wagerr.conf_TEMP
-  echo "port=$PORT" >> wagerr.conf_TEMP
-  echo "masternodeaddr=$IP:55002" >> wagerr.conf_TEMP
-  echo "masternodeprivkey=$PRIVKEY" >> wagerr.conf_TEMP
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> monkey.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> monkey.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> monkey.conf_TEMP
+  echo "rpcport=$RPCPORT" >> monkey.conf_TEMP
+  echo "listen=1" >> monkey.conf_TEMP
+  echo "server=1" >> monkey.conf_TEMP
+  echo "daemon=1" >> monkey.conf_TEMP
+  echo "logtimestamps=1" >> monkey.conf_TEMP
+  echo "maxconnections=256" >> monkey.conf_TEMP
+  echo "masternode=1" >> monkey.conf_TEMP
+  echo "" >> monkey.conf_TEMP
+ 
+  echo "" >> monkey.conf_TEMP
+  echo "port=$PORT" >> monkey.conf_TEMP
+  echo "masternodeaddr=$IP:55002" >> monkey.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> monkey.conf_TEMP
   sudo ufw allow $PORT/tcp
 
-  mv wagerr.conf_TEMP $CONF_DIR/wagerr.conf
+  mv monkey.conf_TEMP $CONF_DIR/monkey.conf
   
   #sh ~/bin/wagerrd_$ALIAS.sh
   
-  cat << EOF > /etc/systemd/system/wagerr_$ALIAS.service
+  cat << EOF > /etc/systemd/system/monkey_$ALIAS.service
 [Unit]
-Description=wagerr_$ALIAS service
+Description=monkey_$ALIAS service
 After=network.target
 [Service]
 User=root
 Group=root
 Type=forking
 #PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
-ExecStart=/usr/local/bin/wagerrd -daemon -conf=$CONF_DIR/wagerr.conf -datadir=$CONF_DIR
-ExecStop=/usr/local/bin/wagerr-cli -conf=$CONF_DIR/wagerr.conf -datadir=$CONF_DIR stop
+ExecStart=/usr/local/bin/monkeyd -daemon -conf=$CONF_DIR/monkey.conf -datadir=$CONF_DIR
+ExecStop=/usr/local/bin/monkey-cli -conf=$CONF_DIR/monkey.conf -datadir=$CONF_DIR stop
 Restart=always
 PrivateTmp=true
 TimeoutStopSec=60s
@@ -243,8 +172,8 @@ EOF
 
   systemctl daemon-reload
   sleep 10
-  systemctl start wagerr_$ALIAS.service
-  systemctl enable wagerr_$ALIAS.service >/dev/null 2>&1
+  systemctl start monkey_$ALIAS.service
+  systemctl enable monkey_$ALIAS.service >/dev/null 2>&1
 
   #(crontab -l 2>/dev/null; echo "@reboot sh ~/bin/wagerrd_$ALIAS.sh") | crontab -
 #	   (crontab -l 2>/dev/null; echo "@reboot sh /root/bin/wagerrd_$ALIAS.sh") | crontab -
